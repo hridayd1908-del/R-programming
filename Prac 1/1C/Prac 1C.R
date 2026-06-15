75e616464c8e7786f919561dee219dd16a470a1b
hridayd1908-del <hridayd1908@gmail.com>
2026-06-15 09:07:34 +0530

    Add files via upload

---
+++ Prac 1/1C/Prac 1C.R
@@ -0,0 +1,9 @@
+Name = c("Rahul", "Priya", "Dinesh", "Paras", "Disha")
+Gender = c("Male", "Female", "Male", "Male", "Female")
+English = c(43, 47, 38, 49, 33)
+Maths = c(45 ,34, 39, 42, 48)
+result = data.frame(Name, Gender, English, Maths)
+print (result)
+"Hriday"
+"SYIT-A"
+"57"
