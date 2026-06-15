08fe853a672d23e539bd0a150499b9f1a325a045
hridayd1908-del <hridayd1908@gmail.com>
2026-06-15 08:57:31 +0530

    Add files via upload

---
+++ Prac 1/1B/Prac 1B.R
@@ -0,0 +1,7 @@
+result=list(c(Name="Rahul", Roll_No = 2, Grade_point=5.2, Grade="D"),c(Name ="Ron",Roll_No = 5, Grade_point=7.6, Grade="A"))
+print(result)
+print(class(result))
+
+"Ajit"
+"SYIT-A"
+"41"
